28d5b0906490b55100da050f770db84c2c81555f
Dave Eric Bautista <pyrodavelapalaz@gmail.com>
2026-09-01 17:50:21 +0800

    second commit

---
+++ .idea/Assignment3.java
@@ -0,0 +1,19 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+        // Assigning each user info to a variable with appropriate data type
+        String firstname = "Dave Eric"; // String for text
+        String lastname = "Bautista"; // String for text
+        String address = "#18, Zone 1, Payas, Sta. Barbara"; // String for text
+        String mobilenumber = "0946 008 6601"; // String because it starts with a 0 and has spaces
+        int age = 18; // Integer for whole numbers
+        double gwa = 98.33; // Double for decimal numbers
+        // Displaying the output
+        System.out.println("=== User Information ===");
+        System.out.println("First Name: " + firstname);
+        System.out.println("Last Name: " + lastname);
+        System.out.println("Address: " + address);
+        System.out.println("Mobile Number: " + mobilenumber);
+        System.out.println("Age: " + age);
+        System.out.println("GWA: " + gwa);
+    }
+}
